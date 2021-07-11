14d8d1af4b2232ab7a69ac1eefc36f32aa6feab2
Robert Schwartz <bschwartz23@gmail.com>
2021-07-10 18:09:59 -0700

    add Route to thought-routes in API routes.  also add routes for post/delete reactions.

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,46 @@
+// Require Express
+const router = require('express').Router();
+
+//Implement Controller Methods
+// ============================================
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+// Set up GET all and POST at /api/thoughts
+// /api/thoughts
+// ============================================
+router
+    // /api/thoughts/
+    .route('/')
+    //provide the name of the controller method as the callback
+    .get(getAllThoughts)
+    .post(createThought);
+    //(don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
+// ============================================
+router
+    // /api/thoughts/:id
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+
+// Set up Create Friend, Delete friend at /api/thoughts/:thoughtId/reactions
+// ============================================
+router
+    // /api/thoughts/:thoughtId/reactions
+    .route('/:id/friends/:friendId')
+    .post(createReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
