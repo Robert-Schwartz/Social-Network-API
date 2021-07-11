649811087a56ed52aac3a2d92fa4873362d37b8b
Robert Schwartz <bschwartz23@gmail.com>
2021-07-10 17:31:16 -0700

    Reaction Schema

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,43 @@
 // ============================
 const { Schema, model } = require('mongoose');
 
-// Thought Schema
+// Reaction Schema
+// ============================
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        reactionId: {
+            // Use Mongoose's ObjectId data type
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    //add toJSON to add getters
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+
+
+// Thought Schema Model
 // ============================
 const ThoughtSchema = new Schema({
     thoughtText: {
