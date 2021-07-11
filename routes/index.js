bdb13a2ea955a389a758d071dfe163dba8ac06dd
Robert Schwartz <bschwartz23@gmail.com>
2021-07-10 17:41:38 -0700

    set up index.js for the routes directory

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+// require express router
+const router = require('express').Router();
+
+
+// Import all of the API routes from /api/index.js
+const apiRoutes = require('./api/index');
+
+// will not be using front end HTML routes yet.  Save for future enhancement
+// const htmlRoutes = require('./html/html-routes');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+// router.use('/', htmlRoutes);
+
+// catch routing errors
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+// Export router
+module.exports = router;
